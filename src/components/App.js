1190672e787abd9aba84b65469682c0156fd3fe7
Dan Constantinescu <dan.constantinescu@softvision.ro>
2017-09-15 14:03:13 +0300

    added react router to the app

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import Popular from './Popular';
+import Nav from './Nav';
+import Home from './Home';
+import Battle from './Battle';
 import '.././App.css';
+
+var ReactRouter = require('react-router-dom');
+var Router = ReactRouter.BrowserRouter;
+var Route = ReactRouter.Route;
+var Switch = ReactRouter.Switch;
+
+
 
 class App extends React.Component {
   render() {
     return (
-      <div className='container'>
-        <Popular />
-      </div>
+      <Router>
+        <div className='container'>
+          <Nav />
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/battle' component={Battle} />
+            <Route path='/popular' component={Popular} />
+            <Route render={function(){
+              return <p>Not Found</p>
+            }} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
